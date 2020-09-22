2e100874fdd465e0ae2e170ec5003d1111ca8ecd
Ethan Wright <wrightet707@gmail.com>
2020-09-21 18:38:32 -0700

    add boiler plate code to make webpack work

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Game from './frontend/game.jsx';
+const Main = () => (
+    <div>
+        {/* <Game /> */}
+        <div className='footer-div'>
+            <a href="https://www.linkedin.com/in/ethan-wright-91a75270/">LinkedIn</a>
+            <a href="https://github.com/wrightet">GitHub</a>
+        </div>
+    </div>
+)
+
+export default Main;
